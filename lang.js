6792d1ac214153bb623db6828d648a2fd1b37e08
Abdulaziz Valiyev <abdulazizvaliev5075@gmail.com>
2025-10-26 22:31:14 +0500

    Fixed langs

--- lang.js
+++ lang.js
@@ -1,4 +1,4 @@
-const langArr = {
+window.langArr = {
     "lng_nav_home": {
         "en": "Home",
         "ru": "Главная",
@@ -29,6 +29,11 @@ const langArr = {
         "ru": "Пробный тест",
         "uz": "Sinov testi"
     },
+    "lng_nav_btn": {
+        "en": "Login",
+        "ru": "Войти",
+        "uz": "Kirish"
+    },
     "lng_header_title": {
         "en": "YOUR FUTURE STARTS HERE",
         "ru": "ВАШЕ БУДУЩЕЕ НАЧИНАЕТСЯ ЗДЕСЬ",
@@ -37,7 +42,7 @@ const langArr = {
     "lng_header_sub_text": {
         "en": "Unlock top IELTS & SAT scores with expert instructors",
         "ru": "Достигайте высоких результатов на IELTS и SAT с опытными преподавателями",
-        "uz": "IELTS va SAT dan yuqori ballarga erishing tajribali o‘qituvchilar bilan"
+        "uz": "IELTS va SAT dan yuqori ballarga erishing tajribali o'qituvchilar bilan"
     },
     "lng_header_sub_text2": {
         "en": "Achieve your goals with us!",
@@ -52,7 +57,7 @@ const langArr = {
     "lng_teacher": {
         "en": "Our Teachers",
         "ru": "Наши преподаватели",
-        "uz": "Bizning o‘qituvchilar"
+        "uz": "Bizning o'qituvchilar"
     },
     "lng_results_title": {
         "en": "Our results",
@@ -82,27 +87,27 @@ const langArr = {
     "lng_contact_info": {
         "en": "Join YES – one of the leading English learning centers in the region!",
         "ru": "Присоединяйтесь к YES — одному из ведущих центров изучения английского языка в регионе!",
-        "uz": "YES markaziga qo‘shiling — mintaqadagi yetakchi ingliz tili markazlaridan biri!"
+        "uz": "YES markaziga qo'shiling — mintaqadagi yetakchi ingliz tili markazlaridan biri!"
     },
     "lng_info_sub": {
         "en": "Start your journey by calling us",
         "ru": "Начните обучение, позвонив нам",
-        "uz": "Bizga qo‘ng‘iroq qilib o‘qishni boshlang"
+        "uz": "Bizga qo'ng'iroq qilib o'qishni boshlang"
     },
     "lng_contact_btn": {
         "en": "Call now!",
         "ru": "Позвонить сейчас!",
-        "uz": "Hozir qo‘ng‘iroq qiling!"
+        "uz": "Hozir qo'ng'iroq qiling!"
     },
     "lng_footer_left_title": {
         "en": "Contact Us",
         "ru": "Свяжитесь с нами",
-        "uz": "Biz bilan bog‘laning"
+        "uz": "Biz bilan bog'laning"
     },
     "lng_footer_left_call": {
         "en": "Call:",
         "ru": "Телефон:",
-        "uz": "Qo‘ng‘iroq:"
+        "uz": "Qo'ng'iroq:"
     },
     "lng_con_name": {
         "en": "+998 (97) 271 77 72",
@@ -127,21 +132,26 @@ const langArr = {
     "lng_about_us_info": {
         "en": "Founded in 2015, YES English Center has become one of the      leading language institutions in the region. Our center is known for delivering high-quality education, professional teachers, and proven strategies for international exams. We proudly hold a record of outstanding student achievements, with our highest IELTS score reaching 8.5. At YES, we believe in personalized learning, discipline, and constant progress — helping each student unlock their true potential.Join us and take your first step toward international success.",
         "ru": "YES English Center был основан в 2015 году и с тех пор стал одним из ведущих центров изучения английского языка в регионе. Наш центр известен высоким качеством обучения, профессиональными преподавателями и эффективной подготовкой к международным экзаменам. Мы гордимся успехами наших учеников — наш наивысший результат IELTS составляет 8.5 баллов. В YES мы придерживаемся индивидуального подхода, дисциплины и постоянного развития. Начни свой путь к международному успеху вместе с нами!",
-        "uz": "YES English Center 2015-yilda tashkil etilgan bo‘lib, mintaqadagi eng yetakchi ingliz tili o‘quv markazlaridan biriga aylangan. Markazimiz yuqori sifatli ta’lim, professional o‘qituvchilar va xalqaro imtihonlarga tayyorlovda samarali metodikasi bilan mashhur. O‘quvchilarimizning yutuqlari biz uchun faxrdir — eng yuqori IELTS natijamiz 8.5 ballni tashkil qiladi. YESda biz individual yondashuv, tartib-intizom va doimiy taraqqiyot tamoyillariga amal qilamiz.Kelajakdagi muvaffaqiyatingizni biz bilan boshlang!"
+        "uz": "YES English Center 2015-yilda tashkil etilgan bo'lib, mintaqadagi eng yetakchi ingliz tili o'quv markazlaridan biriga aylangan. Markazimiz yuqori sifatli ta'lim, professional o'qituvchilar va xalqaro imtihonlarga tayyorlovda samarali metodikasi bilan mashhur. O'quvchilarimizning yutuqlari biz uchun faxrdir — eng yuqori IELTS natijamiz 8.5 ballni tashkil qiladi. YESda biz individual yondashuv, tartib-intizom va doimiy taraqqiyot tamoyillariga amal qilamiz.Kelajakdagi muvaffaqiyatingizni biz bilan boshlang!"
     },
     "lng_feadback_sub":{
         "en":"Real feedback from real students",
         "ru":"Настоящие отзывы от настоящих учеников",
-        "uz":"Haqiqiy ...",
+        "uz":"Haqiqiy talabalardan haqiqiy fikrlar",
     },
      "lng_results_sub":{
         "en":"See how our students achieve their goals",
-        "ru":"Настоящие отзывы от настоящих учеников",
-        "uz":"Haqiqiy ...",
+        "ru":"Посмотрите, как наши студенты достигают своих целей",
+        "uz":"Talabalarimiz maqsadlariga qanday erishganini ko'ring",
     },
      "lng_groups_sub":{
         "en":"Experienced teachers dedicated to your success",
-        "ru":"Настоящие отзывы от настоящих учеников",
-        "uz":"Haqiqiy ...",
+        "ru":"Опытные преподаватели, посвятившие себя вашему успеху",
+        "uz":"Muvaffaqiyatingizga bag'ishlangan tajribali o'qituvchilar",
+    },
+    "lng_footer_center_title":{
+        "en":"Quick Links",
+        "ru":"Быстрые ссылки",
+        "uz":"Tezkor havolalar",
     },
 };
